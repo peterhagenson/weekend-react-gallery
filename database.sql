9cfeaabf3e446da6c850e60431b8732eadc1c579
Peter Hagenson <p.hagenson@gmail.com>
2022-08-13 19:10:28 -0500

    database.sql updated

--- database.sql
+++ database.sql
@@ -7,5 +7,5 @@ CREATE TABLE "galleryItems" (
     likes NUMERIC NOT NULL
     );
     
-   INSERT INTO "galleryItems" (path, description, likes)
-   VALUES ('images/cuencaSmall200.jpeg', 'Cuenca, Ecuador', '0');
+  INSERT INTO "galleryItems" (path, description, likes)
+   VALUES ('https://upload.wikimedia.org/wikipedia/commons/8/8c/Ama_la_Vida_-_Flickr_-_AR_PN_ANDES15_CUENCA_TV_022_%2814584742390%29.jpg', 'Cuenca, Ecuador', '0'), ('https://www.nationsonline.org/gallery/Morocco/Chefchaouen-Morocco.jpg', 'Morocco', '0'), ('https://cdn.britannica.com/61/153461-050-3C2B54D7/Acacia-trees-Taita-Hills-Kenya.jpg', 'Kenya', '0');
